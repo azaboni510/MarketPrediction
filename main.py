fb19bdf912aee5a553e9fd8b668ac04b6ee17822
Aldo <87353080+azaboni510@users.noreply.github.com>
2023-08-07 19:22:26 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import pandas_datareader as web
+import yfinance as yf
 import datetime as dt
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-from pandas_datareader import data as pdr
-
-#load data
-
 company = 'TSLA'
+start = dt.datetime(2023, 1, 25)
+end = dt.datetime(2023, 8, 3)
 
-#HIGH VOLATILITY USE 2 YEARS
-#LOW VALATILITY USE 2+ YEARS FOR TSLA 2018, 1, 11
-start = dt.datetime(2020, 1, 25) #year/date/month
-end = dt.datetime(2021, 6, 12) #year/date/month
+data = yf.Ticker(company).history(period='1d', start=start, end=end)
 
-data = pdr.get_data_yahoo(company, start, end)
-# data = web.DataReader(company, 'yahoo', start, end)
-
-#prepare data
+# Prepare data
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
@@ -33,10 +25,10 @@ y_train = []
 
 for x in range(prediction_days, len(scaled_data)):
     x_train.append(scaled_data[x-prediction_days:x, 0])
-    y_train.append(scaled_data[x,0])
+    y_train.append(scaled_data[x, 0])
 
 x_train, y_train = np.array(x_train), np.array((y_train))
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # BUILD THE MODEL
 
@@ -48,17 +40,17 @@ model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
-model.add(Dense(units=1)) #prediction of the next close
+model.add(Dense(units=1)) # Prediction of the next close
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
 '''Test The Model ACCURACY on Existing DATA'''
 
-#load Test Data
+# Load Test Data
 test_start = dt.datetime(2020, 1, 25)
 test_end = dt.datetime.now()
 
-test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+test_data = yf.Ticker(company).history(period='1d', start=test_start, end=test_end)
 actual_prices = test_data['Close'].values
 
 total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
@@ -79,7 +71,7 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
-#PLOT TEST PREDICTIONS
+# PLOT TEST PREDICTIONS
 plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
 plt.plot(predicted_prices, color='green', label=f"Predicted {company} Price")
 plt.title(f"{company} Share Price ")
@@ -88,18 +80,12 @@ plt.ylabel(f'{company} Share Price')
 plt.legend()
 plt.show()
 
-#PREDICTING NEXT DAY
+# PREDICTING NEXT DAY
 
-real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+real_data = [model_inputs[len(model_inputs) - prediction_days:len(model_inputs), 0]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
 
 prediction = model.predict(real_data)
 prediction = scaler.inverse_transform(prediction)
 print(f"Prediction: {prediction}")
-
-
-
-
-
-
